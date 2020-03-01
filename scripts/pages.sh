154d526fa62d216ed9543ba77022185aa5b2d627
litter <matt@downplaneconsulting.com>
2020-03-01 12:45:31 -0500

    Fixed the pages export script. Proper echo statement

--- scripts/pages.sh
+++ scripts/pages.sh
@@ -10,13 +10,10 @@ DPI=1440
 #Location of generated pages
 PAGE_FILES="~/Desktop/page_*.tif"
 
-echo "Changing calibration files to ${DPI}dpi..."
+echo "Changing exported page files to ${DPI}dpi..."
 
 for f in $PAGE_FILES
 do
-	name=$(basename "$f" .tif)
-	#echo $name
-	echo "Converting $f..."
+	echo "Converting ${f} to ${DPI}dpi..."
 	mogrify $f -units PixelsPerInch -density $DPI
-	rm $f
 done
